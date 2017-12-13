1729355e8c49aed815fefafb19db01a22f2fffe6
Zelalem Tolera <tolerazelalem@gmail.com>
2017-12-13 09:26:54 -0500

    add some css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert("Hello Hungman")
+$(document).ready(function() {
+  var chosenWord = '';
+  var myWord = [];
+  var blankSpaces = [];
+  var letterIndex = [];
+  var arrayReplace = [];
+  var previousGuesses = [];
+  var lives = 6;              
+  var possibleWords = ['DO', 'FOR', 'CHAR', 'CLASS','DELETE'];
+  // ["boolean", "break", "byte", "case", "catch", "abstract",  "arguments",  "await" "const",  "continue", "debugger", "default",  "delete",  "double",  "else", "enum", "eval", "export", "extends",  "false",  "final", "finally", "float",  "for",  "function", "goto", "if", "implements", "import", "in", "instanceof", "int",  "interface", "let", "long", "native", "new", "null",  "package",  "private",  "protected", "public",  "return", "short",  "static", "super", "switch",  "synchronized", "this", "throw",  "throws", "transient",  "true", "try",  "typeof", "var",  "void", "volatile", "while",  "with", "yield"]
+
+  
+
+}); 
