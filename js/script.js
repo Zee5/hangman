618d9febf90240e62e1dc54cc59eefe74c4644dc
Zelalem Tolera <tolerazelalem@gmail.com>
2017-12-13 12:00:28 -0500

    some changes

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   var arrayReplace = [];
   var previousGuesses = [];
   var lives = 6;              
-  var possibleWords = ['DO', 'FOR', 'CHAR', 'CLASS','DELETE'];
+  var possibleWords = ['DO', 'FOR', 'CHAR', 'CLASS','ARGUMRNTS', 'DELETE'];
   // ["boolean", "break", "byte", "case", "catch", "abstract",  "arguments",  "await" "const",  "continue", "debugger", "default",  "delete",  "double",  "else", "enum", "eval", "export", "extends",  "false",  "final", "finally", "float",  "for",  "function", "goto", "if", "implements", "import", "in", "instanceof", "int",  "interface", "let", "long", "native", "new", "null",  "package",  "private",  "protected", "public",  "return", "short",  "static", "super", "switch",  "synchronized", "this", "throw",  "throws", "transient",  "true", "try",  "typeof", "var",  "void", "volatile", "while",  "with", "yield"]
 
   $('#beginGame').on('click', startGameClick) 
@@ -22,7 +22,7 @@ $(document).ready(function() {
     $('#hidden').append((blankSpaces));      // puts all the _'s in the dom in the div with the 'hidden' id
     $('#livesLeftNumber').text(lives);  // displays lives on the dom when start game is clicked
     showHangedMan(lives);   
-    // alert(chosenWord);   
+    
   }; 
 
 
